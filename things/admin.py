38420b4fe54491dd5afe426f5911c85927d396fe
Munye Jeilani <munye.jeilani@kcl.ac.uk>
2023-12-08 13:00:54 +0000

    Made changes  in model

--- things/admin.py
+++ things/admin.py
@@ -1,12 +1,3 @@
 from django.contrib import admin
 
-from django.db import models
-from django.core.validators import MinValueValidator, MaxValueValidator
-
-
-class Thing(models.Model):
-    name = models.CharField(max_length=30, unique=True, blank=False)  
-    description = models.TextField(blank=True, max_length=120) 
-    quantity = models.IntegerField(default=0, validators=[MinValueValidator(0), MaxValueValidator(100)])  
-    def __str__(self):
-        return self.name
+# Register your models here.
