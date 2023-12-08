83ab976fc00f3b3168c139847e16562c14cc1140
Munye Jeilani <munye.jeilani@kcl.ac.uk>
2023-12-08 15:26:26 +0000

    Made changes  in model

--- things/models.py
+++ things/models.py
@@ -2,9 +2,15 @@ from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 
 
+description_length_validator = RegexValidator(
+    regex=r'^.{0,120}$', 
+    message='Description must have a maximum of 120 characters.',
+)
+
 class Thing(models.Model):
-    name = models.CharField(max_length=30, unique=True, blank=False)  
-    description = models.TextField(blank=True, max_length=120) 
-    quantity = models.IntegerField(default=0, validators=[MinValueValidator(0), MaxValueValidator(100)])  
+    name = models.CharField(max_length=30, unique=True, blank=False)
+    description = models.TextField(blank=True, validators=[description_length_validator])
+    quantity = models.IntegerField(default=0, validators=[MinValueValidator(0), MaxValueValidator(100)])
+
     def __str__(self):
         return self.name
